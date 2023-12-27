03d44b8b1cb291bf16d5217de9a0fd71c0ad03bf
Chris <138630942+CrisCo116@users.noreply.github.com>
2023-12-27 14:57:49 -0800

    models may be finished

---
+++ models/thought.js
@@ -0,0 +1,76 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+// Validation Middleware
+const reactionBodyValidator = [
+  ({ length }) => length <= 280,
+  'Reaction must be less than 280 characters.',
+];
+
+const thoughtTextValidator = [
+  ({ length }) => length <= 280,
+  'Thought must be less than 280 characters.',
+];
+
+// Reaction Schema
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      validate: reactionBodyValidator,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+    },
+  },
+  {
+    ...commonOptions,
+  }
+);
+
+// Thought Schema
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      validate: thoughtTextValidator,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    ...commonOptions,
+  }
+);
+
+// Virtual Field for Reaction Count
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+// Create the Thought model using the ThoughtSchema
+const Thought = model('thought', thoughtSchema);
+
+module.exports = thought;
---
+++ models/user.js
@@ -0,0 +1,50 @@
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: 'Username is Required',
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: 'Email is Required',
+      unique: true,
+      match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Reaction',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = user;
